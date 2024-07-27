9dedeb23c597936765517d24d2aff95e1de935ea
GjoeKariuki <githaigageorge96@gmail.com>
2024-07-27 10:17:52 +0300

    test passed

--- tests/nginx-tests.py
+++ tests/nginx-tests.py
@@ -2,25 +2,13 @@
 
 import requests
 
-
-
-
-def test_nginx_proxy():
-    response  = requests.get("http://localhost/")
-    assert response.status_code == 200, "Nginx proxy"
-
-
 def test_nginx_proxy_to_orthanc():
-    response = requests.get('http://localhost/orthanc')
+    response = requests.get('http://localhost:8053/', timeout=10)
     assert response.status_code == 200
 
 def test_nginx_proxy_to_ohif():
-    response = requests.get('http://localhost/ohif')
+    response = requests.get('http://localhost/ohif/', timeout=10)
     assert response.status_code == 200
 
-def test_nginx_proxy_ver():
-    response = requests.get('http://localhost/')
-    assert response.status_code == 200
-    assert 'Orthanc' in response.text or 'OHIF' in response.text
 
 
